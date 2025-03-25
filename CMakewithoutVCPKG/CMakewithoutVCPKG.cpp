7a1f28b41d23c9f015b2f88d9ec24a008ead0878
Chang Min Bark <changminbark@gmail.com>
2025-03-24 23:56:15 -0400

    added library manually to cmake project

---
+++ CMakewithoutVCPKG/CMakewithoutVCPKG.cpp
@@ -0,0 +1,18 @@
+﻿// CMakewithoutVCPKG.cpp : Defines the entry point for the application.
+//
+
+#include "CMakewithoutVCPKG.h"
+#include <plog/Log.h>
+#include <plog/Initializers/RollingFileInitializer.h>
+
+using namespace std;
+
+int main()
+{
+	// Test out library
+	plog::init(plog::debug, "C:\\Users\\chang\\Desktop\\Github-Projects\\cpp-practice\\CMakewithoutVCPKG\\log\\Logfile.txt");
+	PLOGD << "main called";
+
+	cout << "Hello CMake." << endl;
+	return 0;
+}
